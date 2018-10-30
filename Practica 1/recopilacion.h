8eb99819e687fa9ed6b187a0ba51190f8249f04e
Andrés Gavín Murillo <37375662+agavinm@users.noreply.github.com>
2018-10-30 13:59:30 +0100

    Añadida función introducir
    Pequeños cambios y comentarios

--- Practica 1/recopilacion.h
+++ Practica 1/recopilacion.h
@@ -1,65 +1,128 @@
-//
-// Created by jorge on 29/10/2018.
-//
+//*****************************************************************
+// Archivo: recopilacion.h
+// Autor:   Andrés Gavín Murillo 716358
+// Autor:   Jorge ...
+// Fecha:   Noviembre 2018
+// Coms:    EDA - Práctica 1 - TAD Recopilación
+//*****************************************************************
 
 #ifndef RECOPILACION_H
 #define RECOPILACION_H
-template <typename C,typename V> struct Recopilacion;
-template <typename C,typename V> Recopilacion<C,V> crear();
-template <typename C, typename V> int cardinal(Recopilacion<C,V> &r);
-template <typename C, typename V> int tamanyoTotal(Recopilacion<C,V> &r);
-template <typename C, typename V> bool existe(Recopilacion<C,V> &r, C &clave);
 
-template <typename C,typename V> struct Recopilacion{
-    friend Recopilacion crear<C,V>();
-    friend int cardinal<C,V>(Recopilacion &r);
-    friend int tamanyoTotal<C,V>(Recopilacion &r);
-    friend bool existe<C,V>(Recopilacion &r,C &clave);
+// Interfaz del TAD Recopilación. Pre-declaraciones:
+template<typename C, typename V> struct Recopilacion;
+template<typename C, typename V> Recopilacion<C,V> crear ();
+template<typename C, typename V> int cardinal (Recopilacion<C,V> &r);
+template<typename C, typename V> int tamanyoTotal (Recopilacion<C,V> &r);
+template<typename C, typename V> bool existe (Recopilacion<C,V> &r, C &k);
+template<typename C, typename V> void introducir (Recopilacion<C,V> &r, C &k, V &v);
+
+// Declaración
+template<typename C, typename V>
+struct Recopilacion {
+    friend Recopilacion crear<C,V> ();
+    friend int cardinal<C,V> (Recopilacion &r);
+    friend int tamanyoTotal<C,V> (Recopilacion &r);
+    friend bool existe<C,V> (Recopilacion &r, C &k);
+    friend void introducir<C,V> (Recopilacion<C,V> &r, C &k, V &v);
 private:
-    struct Par{
+    struct Par {
         C clave;
         V valor;
-        int puesto;
-        Par * anterior;
-        Par * siguiente;
+        Par *siguiente;
     };
-    Par * inicial;
+    Par *inicial;
+    Par *final;
     int numDatos;
     int tamanyo;
-    Par * iterador;
+    Par *iterador;
 };
 
-template <typename C,typename V> Recopilacion<C,V> crear(){
+// Implementación
+template<typename C, typename V>
+Recopilacion<C,V> crear () {
     Recopilacion<C,V> r;
-    r.numDatos=0;
-    r.tamanyo=0;
-    r.inicial=nullptr;
-    r.iterador=nullptr;
+    r.numDatos = 0;
+    r.tamanyo = 0;
+    r.inicial = nullptr;
+    r.final = nullptr;
+    r.iterador = nullptr;
+    return r;
 }
 
-template <typename C, typename V> int cardinal(Recopilacion<C,V> &r){
+template<typename C, typename V>
+int cardinal (Recopilacion<C,V> &r) {
     return r.numDatos;
 }
 
-template <typename C, typename V> int tamanyoTotal(Recopilacion<C,V> &r){
+template<typename C, typename V>
+int tamanyoTotal (Recopilacion<C,V> &r) {
     return r.tamanyo;
 }
 
-template <typename C, typename V> bool existe(Recopilacion<C,V> &r, C &clave){
-    if(r.tamanyo==0){
+template<typename C, typename V>
+bool existe (Recopilacion<C,V> &r, C &k) {
+    if (r.tamanyo == 0) {
+        // r está vacía
         return false;
     }
-    typename Recopilacion<C,V>::Par *busqueda= new typename Recopilacion<C,V>::Par;
-    busqueda= r.inicial;
-    while(busqueda!=nullptr){
-        if(busqueda->clave==clave){
+
+    typename Recopilacion<C,V>::Par *busqueda = r.inicial;
+
+    while (busqueda != nullptr) {
+        // r no está vacía
+        if (busqueda->clave == k) {
+            // existe un Par con clave k
             return true;
         }
-        else{
-            busqueda=busqueda->siguiente;
+        else {
+            busqueda = busqueda->siguiente;
         }
     }
+
+    // no existe un Par con clave k
     return false;
 }
 
-#endif RECOPILACION_H
+template<typename C, typename V>
+void introducir (Recopilacion<C,V> &r, C &k, V &v) {
+    typename Recopilacion<C,V>::Par *aux = r.inicial;
+    bool encontrado = false;
+
+    while (aux != nullptr && !encontrado) {
+        // r no está vacía
+        if (aux->clave == k) {
+            // ya existe un Par con clave k
+            encontrado = true;
+            r.tamanyo -= aux->valor.tamanyo(); // Operación de V
+            aux->valor = v;
+            r.tamanyo += aux->valor.tamanyo(); // Operación de V
+        }
+        else {
+            aux = aux->siguiente;
+        }
+    }
+
+    if (!encontrado) {
+        // no existe un Par con clave k ó r está vacía
+        aux = new typename Recopilacion<C,V>::Par;
+        aux->valor = v;
+        aux->clave = k;
+        aux->siguiente = nullptr;
+        r.numDatos++;
+        r.tamanyo = v.tamanyo(); // Operación de V
+
+        if (r.inicial == nullptr) {
+            // r está vacía
+            r.inicial = aux;
+            r.final = aux;
+        }
+        else {
+            // r no está vacía
+            r.final->siguiente = aux;
+            r.final = aux;
+        }
+    }
+}
+
+#endif // fin de recopilacion.h
